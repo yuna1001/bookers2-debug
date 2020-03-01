9cc5779c03c96567686e8e7097d72736192bbeeb
yuna1001 <hegapoo1001@gmail.com>
2020-03-01 08:21:42 +0000

    [Fix] Syntaxエラー出なくなりました

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def index
   	@users = User.all #一覧表示するためにUserモデルのデータを全て変数に入れて取り出す。
   	@book = Book.new #new bookの新規投稿で必要（保存処理はbookコントローラー側で実施）
+  end
 
   def edit
   	@user = User.find(params[:id])
@@ -30,10 +31,10 @@ class UsersController < ApplicationController
   end
 
   #url直接防止　メソッドを自己定義してbefore_actionで発動。
-   def baria_user
+  def baria_user
   	unless params[:id].to_i == current_user.id
   		redirect_to user_path(current_user)
-  	end
-   end
+  end
+  end
 
 end
