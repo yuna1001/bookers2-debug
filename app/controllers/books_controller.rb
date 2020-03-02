f160ce0885647f94f3d9928ff9ba1faf0b6e9640
yuna1001 <hegapoo1001@gmail.com>
2020-03-02 13:45:34 +0000

    [Fix] books編集ページの修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -23,9 +23,10 @@ class BooksController < ApplicationController
 
   def edit
   	@book = Book.find(params[:id])
+    if @book.user.id != current_user.id
+       redirect_to books_path
+    end
   end
-
-
 
   def update
   	@book = Book.find(params[:id])
